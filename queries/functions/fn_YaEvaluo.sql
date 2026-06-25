452bc80b0e9b142640cfdd9600ccd217966bbe35
sea2horses <thefuzegamer06@gmail.com>
2026-06-24 23:06:30 -0600

    añadir `fn_YaEvaluo`

---
+++ queries/functions/fn_YaEvaluo.sql
@@ -0,0 +1,28 @@
+---------------------------------------------------------------
+-- 15. FUNCIÓN: VALIDAR SI YA EVALUÓ
+-- Retorna 1 si el estudiante ya evaluó la asignación.
+---------------------------------------------------------------
+
+CREATE OR ALTER FUNCTION dbo.fn_YaEvaluo
+(
+    @EstudianteID INT,
+    @AsignacionID INT
+)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @Resultado BIT = 0;
+
+    IF EXISTS (
+        SELECT 1
+        FROM dbo.Evaluaciones
+        WHERE EstudianteID = @EstudianteID
+          AND AsignacionID = @AsignacionID
+    )
+    BEGIN
+        SET @Resultado = 1;
+    END
+
+    RETURN @Resultado;
+END;
+GO

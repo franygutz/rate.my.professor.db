8d9f8a909d0e9e923490a5f5f4ea7b3320bb26f0
sea2horses <thefuzegamer06@gmail.com>
2026-06-24 23:07:27 -0600

    añadir `sp_ReporteTopProfesores`

---
+++ queries/stored_procedures/sp_ReporteTopProfesores.sql
@@ -0,0 +1,33 @@
+---------------------------------------------------------------
+-- 17. PROCEDIMIENTO: REPORTE TOP PROFESORES
+-- Devuelve los mejores profesores según promedio general.
+---------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE dbo.sp_ReporteTopProfesores
+    @Top INT = 5
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF @Top <= 0
+    BEGIN
+        THROW 50005, 'El parámetro @Top debe ser mayor que cero.', 1;
+    END;
+
+    SELECT TOP (@Top)
+        ProfesorID,
+        Profesor,
+        Departamento,
+        CantidadEvaluaciones,
+        PromedioCalidadGeneral,
+        PromedioClaridad,
+        PromedioDificultad,
+        PromedioUtilidad,
+        PromedioGeneral
+    FROM dbo.vw_RankingProfesores
+    WHERE CantidadEvaluaciones > 0
+    ORDER BY
+        PromedioGeneral DESC,
+        CantidadEvaluaciones DESC;
+END;
+GO
